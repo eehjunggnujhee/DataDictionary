40dac5808094f8fe2086b31d732e23519c6f19e3
jm <eehjunggnujhee@gmail.com>
2022-01-29 18:44:48 -0800

    prepping for docs

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ language = 'apex'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','make.bat','Makefile']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
